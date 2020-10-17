dd448ebd27f157f67e03ebc45b73b8d5bb5d2ef2
yangxvlin <1768528843@qq.com>
2020-10-17 16:35:34 +1100

    add symbol table

--- Roo/SymbolTable.hs
+++ Roo/SymbolTable.hs
@@ -6,4 +6,21 @@
 -----------------------------------------------------------
 module SymbolTable where
 
-data SymbolTable = SymbolTable ()
+import Control.Monad.State
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
+import Text.Parsec.Pos
+import RooAST
+
+-- a (global) "type table" which holds information about type aliases and the 
+-- composite types then name;
+data TypeTable 
+  = TypeTable
+    -- map of array name with array size and 
+    --                        data type (bool or integer or record)
+    { arrayType :: Map String (Int, DataType) 
+    -- map of record name with field declaration 
+    --                             (map of field's name with bool or integer) 
+    --                         and number of fields
+    , recordType :: Map String ((Map String BaseType), Int)
+    }
