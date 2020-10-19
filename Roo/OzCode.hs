c92302794e904e6cb8ac6dfac8a87c333caf0b35
wenruiz <wenruiz@student.unimelb.edu.au>
2020-10-20 09:36:58 +1100

    corrected

--- Roo/OzCode.hs
+++ Roo/OzCode.hs
@@ -1,7 +1,7 @@
 -----------------------------------------------------------
 -- COMP90045 Programming Language Implementation Project --
 --                     Roo Compiler                      --
---  Implemented by Xulin Yangm, Wenrui Zhang             --
+--  Implemented by Xulin Yang, Wenrui Zhang              --
 --  Implemented by Team: GNU_project                     --
 -----------------------------------------------------------
 
@@ -12,85 +12,85 @@ writeCode _ = "writeCode"
 
 type Register = Int
 
--- data stackInstruction
---     = PushStackFrame Int
---     | PopStachFrame Int
---     | Store Int Register
---     | Load Register Int
---     | LoadAddress Register Int
---     | LoadIndirect Register Register
---     | StoreIndirect Register Register
---     deriving (Show, Eq)
+data StackInstruction
+    = PushStackFrame Int
+    | PopStachFrame Int
+    | Store Int Register
+    | Load Register Int
+    | LoadAddress Register Int
+    | LoadIndirect Register Register
+    | StoreIndirect Register Register
+    deriving (Show, Eq)
 
--- data ConstantInstruction
---     = IntConst Register Int
---     | RealConst Register Float
---     | StringConst Register String
---     deriving (Show, Eq)
+data ConstantInstruction
+    = IntConst Register Int
+    | RealConst Register Float
+    | StringConst Register String
+    deriving (Show, Eq)
 
--- data OpType 
---     = OpInt
---     | OpReal
---     deriving (Show, Eq)
+data OpType 
+    = OpInt
+    | OpReal
+    deriving (Show, Eq)
 
--- data ArithmeticInstruction
---     = binary Add OpType Register Register Register
---     | AddOff Register Register Register
---     | binary Sub OpType Register Register Register
---     | SubOff Register Register Register
---     | binary Mul OpType Register Register Register
---     | binary Div OpType Register Register Register
---     | prefix Neg OpType Register Register
---     deriving (Show, Eq)
+data ArithmeticInstruction
+    = Add OpType Register Register Register
+    | AddOff Register Register Register
+    | Sub OpType Register Register Register
+    | SubOff Register Register Register
+    | Mul OpType Register Register Register
+    | Div OpType Register Register Register
+    | Neg OpType Register Register
+    deriving (Show, Eq)
 
--- data ComparisonInstruction
---     = binary Eq OpType Register Register Register
---     | binary Ne OpType Register Register Register
---     | binary Gt OpType Register Register Register
---     | binary Ge OpType Register Register Register
---     | binary Lt OpType Register Register Register
---     | binary Le OpType Register Register Register
---     deriving (Show, Eq)
+data ComparisonInstruction
+    = Eq OpType Register Register Register
+    | Ne OpType Register Register Register
+    | Gt OpType Register Register Register
+    | Ge OpType Register Register Register
+    | Lt OpType Register Register Register
+    | Le OpType Register Register Register
+    deriving (Show, Eq)
 
--- data LogicInstruction
---     = LogicAnd Register Register Register
---     | LogicOr Register Register Register
---     | LogicNot Register Register
---     deriving (Show, Eq)
+data LogicInstruction
+    = LogicAnd Register Register Register
+    | LogicOr Register Register Register
+    | LogicNot Register Register
+    deriving (Show, Eq)
 
--- data OperationInstruction
---     = ArithmeticInstruction
---     | ComparisonInstruction
---     | LogicInstruction
---     | Int2real Register Register
---     | Move Register Register
---       deriving (Show, Eq)
+data OperationInstruction
+    = ArithmeticInstruction
+    | ComparisonInstruction
+    | LogicInstruction
+    | Int2real Register Register
+    | Move Register Register
+      deriving (Show, Eq)
 
--- data BranchInstruction
---     = Cond Bool Register String
---     | Uncond String
---     deriving (Show, Eq)
+data BranchInstruction
+    = Cond Bool Register String
+    | Uncond String
+    deriving (Show, Eq)
 
--- data ProcedureInstruction
---     = Call String
---     | CallBuiltIn String
---     | Return
---     | Halt
---     deriving (Show, Eq)
+data ProcedureInstruction
+    = Call String
+    | CallBuiltIn String
+    | Return
+    | Halt
+    deriving (Show, Eq)
 
--- data DebugInstruction
---     = DebugReg Register
---     | DebugSlot Int
---     | DebugStack
---     deriving (Show, Eq)
+data DebugInstruction
+    = DebugReg Register
+    | DebugSlot Int
+    | DebugStack
+    deriving (Show, Eq)
 
--- data Instruction
---     = stackInstruction
---     | ConstantInstruction
---     | OperationInstruction
---     | BranchInstruction
---     | ProcedureInstruction
---     | DebugInstruction
---     | Comment String
---     | Label String
---     deriving (Show, Eq)
+data Instruction
+    = StackInstruction
+    | ConstantInstruction
+    | OperationInstruction
+    | BranchInstruction
+    | ProcedureInstruction
+    | DebugInstruction
+    | Comment String
+    | Label String
+    deriving (Show, Eq)
