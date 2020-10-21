87310fac7220e6fe5ff5c5a70384b2cfd30151a4
Chao Zhang <c.zhang93@student.unimelb.edu.au>
2020-10-21 18:22:16 +1100

    write and writeln code generator complete

--- Roo/Codegen.hs
+++ Roo/Codegen.hs
@@ -18,6 +18,8 @@ import Data.Either
 
 type Consequence = Either String [OzInstruction]
 
+data Btype = Int | Bool | String
+
 ozCode :: SymTable -> Program -> Consequence
 ozCode st prog = evalStateT (codeGeneration prog) st
 
@@ -31,35 +33,71 @@ codeGeneration _
             mapM_ appendInstruction generatedCode
             -- starts with main procedure
             (_, mainProc) <- getProcedure "main"
-            -- codes <- generateProcedure mainProc
+            generateProcedure mainProc
 
             st <- get
             return $ instructions st
 
--- generateProcedure :: Procedure -> SymTableState [OzInstruction]
--- generateProcedure p@(Procedure (ProcedureHeader procID _) (ProcedureBody _ stmts))
---     =
---         do
---             let generatedCode = [Label procID] 
+generateProcedure :: Procedure -> SymTableState ()
+generateProcedure p@(Procedure (ProcedureHeader procID _) (ProcedureBody _ stmts))
+    =
+        do
+            appendInstruction $ Label procID 
 
---             -- 
---             pushLocalVariableTable
---             -- insert procedure's variable info to local variable table
---             insertProcedureVariable p
---             -- generate code of procedure's statements
---             -- reg <- getRegisterCounter
---             -- generatedCode <- generatedCode ++ [StackInstruction $ Load $ reg 10] 
---             let code = concat (mapM generateStatement stmts)
+            pushLocalVariableTable
+            -- insert procedure's variable info to local variable table
+            insertProcedureVariable p
+            -- generate code of procedure's statements
+            -- reg <- getRegisterCounter
+            -- generatedCode <- generatedCode ++ [StackInstruction $ Load $ reg 10] 
+            mapM_ generateStatement stmts
 
---             popLocalVariableTable
+            popLocalVariableTable
 
---             return $ generatedCode 
+           
 
--- generateStatement :: Stmt -> SymTableState [OzInstruction]
--- generateStatement _ 
---     = 
---         do
---             return [Comment "this is a statment"]
+generateStatement :: Stmt -> SymTableState ()
+generateStatement (Write exp)
+    = 
+        do
+            appendInstruction $ Comment $ "Write " ++ show exp
+            let bType = getType exp
+            reg <- getRegisterCounter
+            loadExp reg exp
+            let cmd = case bType of Int -> "print_int"
+                                    Bool -> "print_bool"
+                                    String -> "print_string"
+            appendInstruction $ ProcedureInstruction $ ICallBuiltIn cmd
+            setRegisterCounter reg
+
+generateStatement (Writeln exp)   
+    = 
+        do 
+            appendInstruction $ Comment $ "Writeln " ++ show exp
+            let bType = getType exp
+            reg <- getRegisterCounter
+            loadExp reg exp
+            let cmd = case bType of Int -> "print_int"
+                                    Bool -> "print_bool"
+                                    String -> "print_string"
+            appendInstruction $ ProcedureInstruction $ ICallBuiltIn cmd
+            appendInstruction $ ProcedureInstruction $ ICallBuiltIn "print_newline"
+            setRegisterCounter reg
+-- need to update
+generateStatement _ 
+    = appendInstruction $ Comment "this is an undefined statment"
+
+-- load an expression to the given register
+loadExp :: Int -> Exp -> SymTableState ()
+loadExp reg (BoolConst vl) 
+    = appendInstruction $ ConstantInstruction $ OzIntConst reg $ boolToInt vl
+loadExp reg (IntConst vl)
+    = appendInstruction $ ConstantInstruction $ OzIntConst reg vl
+loadExp reg (StrConst vl)
+    = appendInstruction $ ConstantInstruction $ OzStringConst reg vl
+loadExp reg _ 
+    = appendInstruction $ Comment "this is an undefined expression"
+
 
 -- -- transfer operation from Exp in AST to Oz instruction
 -- getOzInstruction :: Exp -> OzInstruction
@@ -73,3 +111,29 @@ codeGeneration _
 -- getOzInstruction Op_large  = Gt
 -- getOzInstruction Op_large_eq  = Ge
 -- getOzInstruction Op_neg  = Neg
+
+
+getType :: Exp -> Btype
+getType (BoolConst _) = Bool 
+getType (IntConst _) = Int
+getType (StrConst _) = String
+getType (Op_or _ _) = Bool 
+getType (Op_and _ _) = Bool
+getType (Op_eq _ _) = Bool 
+getType (Op_neq _ _) = Bool
+getType (Op_less _ _) = Bool
+getType (Op_less_eq _ _) = Bool
+getType (Op_large _ _) = Bool
+getType (Op_large_eq _ _) = Bool
+getType (Op_add _ _) = Int
+getType (Op_sub _ _) = Int
+getType (Op_mul _ _) = Int
+getType (Op_div _ _) = Int
+getType (Op_not _) = Bool
+getType (Op_neg _) = Int
+-- need to update later!!!!!!!!!!!!!
+getType (Lval _) = Int
+
+boolToInt :: Bool -> Int
+boolToInt True = 1
+boolToInt False = 0
